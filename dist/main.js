a5db797723190939276edc18a83886a3dba21ea9
Stevenz10 <49377360+Stevenz10@users.noreply.github.com>
2022-10-31 22:21:34 -0500

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,6 @@
 import './style.css';
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/build/three.module.js';
 
-
 // Setup
 
 const scene = new THREE.Scene();
