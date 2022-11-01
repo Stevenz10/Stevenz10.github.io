1d9a3f360851a6163449f46170f3e707f64359a1
Stevenz10 <49377360+Stevenz10@users.noreply.github.com>
2022-10-31 22:19:43 -0500

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,6 @@
 import './style.css';
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/build/three.module.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 
 // Setup
 
